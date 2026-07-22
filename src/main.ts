c509d71118eee2d00a146438efc473f2391cd6ee
maxxxsalvatore <email_github_maxxxsalvatore_maxsalvatore1666@gmail.com>
2026-07-22 16:30:31 +0700

    Add Swagger UI documentation on /docs

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'https://trackinginternal.com',
-      'https://www.trackinginternal.com',
-      'http://localhost:3000',
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  // Enable CORS agar bisa diakses dari frontend mana pun
+  app.enableCors();
+
+  // Konfigurasi Swagger UI
+  const config = new DocumentBuilder()
+    .setTitle('GeoTrack Backend API')
+    .setDescription('Dokumentasi API untuk Geotrack Operations & Authentication')
+    .setVersion('1.0')
+    .addBearerAuth() // Tambahkan dukungan JWT Bearer token
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   const port = process.env.PORT || 8080;
   await app.listen(port);
-  console.log(`🚀 Server running on port: ${port}`);
+  console.log(`Application is running on port: ${port}`);
 }
 bootstrap();
